c88a9b8b3615c9ef77295fcbe1c9d92d286de7fa
Sean Oughton <seanoughton@gmail.com>
2018-05-15 22:05:24 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20180515220232) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.integer  "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
