136fe96dc359588041f7661498c51859fcb715a2
Sean Oughton <seanoughton@gmail.com>
2018-05-15 22:18:20 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,10 @@ class SessionsController < ApplicationController
     @user = User.find_or_create_by(uid: auth['uid']) do |u|
      u.name = auth['info']['name']
      u.email = auth['info']['email']
-   end
+    end
+    session[:user_id] = @user.id
+
+    render 'welcome/home'
   end
 
   def auth
